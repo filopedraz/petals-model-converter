364924e7b7a18e8f9862ff78671469cd51987fa3
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-09-07 19:36:27 +0200

    low mem script

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Reshard any HF Model for Petals Decentralized Inference Engine.
 
 ## Getting Started
 
+## Normal Script Version
+
 ### 1. Change the variables
 
 ```python
@@ -19,6 +21,44 @@ python main.py
 ```
 
 ### 3. Copy Files
+
+When the conversion has been completed, copy the following files from the official model Repository and push the new model in HF.
+
+- `.gitattributes`
+- `config.json`
+- `generation_config.json`
+- `special_tokens_map.json`
+- `tokenizer_config.json`
+- `tokenizer.json`
+- `tokenizer.model`
+
+## Low Memory Consumption Version
+
+### 1. Clone the HF Repository
+
+```bash
+git lfs install
+huggingface-cli lfs-enable-largefiles .
+git clone https://huggingface.co/premai-io/CodeLlama-34b-Instruct-hf
+```
+
+### 2. Change the variables
+
+```python
+input_folder = "models/example/CodeLlama-34b-Instruct-hf"
+output_folder = "models/CodeLlama-34b-Instruct-hf"
+
+number_of_hidden_layers = 48
+number_of_current_shards = 7
+```
+
+### 3. Run the script
+
+```python
+python main_low_mem.py
+```
+
+### 4. Copy Files
 
 When the conversion has been completed, copy the following files from the official model Repository and push the new model in HF.
 
--- main_low_mem.py
+++ main_low_mem.py
@@ -1,9 +1,8 @@
 import torch
 import json
 import logging
-import requests
 import os
-import io
+
 from safetensors.torch import save_file
 
 logging.basicConfig(
@@ -12,62 +11,59 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-model_name = "codellama/CodeLlama-34b-Instruct-hf"
-
 input_folder = "models/example/CodeLlama-34b-Instruct-hf"
 output_folder = "models/CodeLlama-34b-Instruct-hf"
+
 number_of_hidden_layers = 48
 number_of_current_shards = 7
 
 os.makedirs(output_folder, exist_ok=True)
 
-k = 1
+total_size = 0
 weight_map = {}
-for i in range(1, number_of_current_shards + 1):
-    chunk_url = f"https://huggingface.co/{model_name}/resolve/main/pytorch_model-0000{i}-of-0000{number_of_current_shards}.bin"
-    
-    response = requests.get(chunk_url)
-    response.raise_for_status()
-    
-    buffer = io.BytesIO(response.content)
-    chunk = torch.load(buffer, map_location="cpu")
+for k in range(number_of_hidden_layers):
+    shard_name = f"model_000{k+1}-of-000{number_of_hidden_layers+1}.safetensors"
 
-    for name, tensor in chunk.items():
-        print(name)
-        print(tensor.dtype)
-        print(tensor.shape)
-        if name.startswith(f'model.layers.{i}.'):
-            converted_tensor = tensor.to(torch.bfloat16)
-            shard_name = f"model_000{k}-of-000{number_of_hidden_layers+1}.safetensors"
-            save_file({name: converted_tensor}, f"./{output_folder}/{shard_name}", metadata={"format": "pt"})
-            weight_map[name] = shard_name
+    for i in range(1, number_of_current_shards + 1):
+        chunk = torch.load(f"{input_folder}/pytorch_model-0000{i}-of-0000{number_of_current_shards}.bin", map_location="cpu")
 
-            k = k + 1
+        shard_tensors = {}
+        for name, tensor in chunk.items():
+            if name.startswith(f'model.layers.{k}.'):
+                converted_tensor = tensor.to(torch.bfloat16)
+                total_size += converted_tensor.numel() * 2
             
-            del tensor
-            del converted_tensor
+                weight_map[name] = shard_name
+                shard_tensors[name] = converted_tensor
+        
+        if len(shard_tensors.keys()) > 0:
+            save_file(shard_tensors, f"./{output_folder}/{shard_name}", metadata={"format": "pt"})
+            logging.debug(f"Saved {' '.join(shard_tensors.keys())} in shard {shard_name}.")
+        
+        del chunk
+        del shard_tensors
 
+shard_name = f"model_000{number_of_hidden_layers+1}-of-000{number_of_hidden_layers+1}.safetensors"
+for i in range(1, number_of_current_shards + 1):
+    chunk = torch.load(f"{input_folder}/pytorch_model-0000{i}-of-0000{number_of_current_shards}.bin", map_location="cpu")
+
+    shard_tensors = {}
+    for name, tensor in chunk.items():
+        if name not in weight_map:
+            converted_tensor = tensor.to(torch.bfloat16)
+            total_size += converted_tensor.numel() * 2
+        
+            weight_map[name] = shard_name
+            shard_tensors[name] = converted_tensor
+    
+    if len(shard_tensors.keys()) > 0:
+        save_file(shard_tensors, f"./{output_folder}/{shard_name}", metadata={"format": "pt"})
+        logging.debug(f"Saved {' '.join(shard_tensors.keys())} in shard {shard_name}.")
+    
     del chunk
-    logging.debug(f"Processed shard {i} out of {number_of_current_shards}.")
+    del shard_tensors
 
-logging.debug(f"Created weight map using {len(weight_map.keys())} blocks.")
-exit()
-
-total_size = sum(p.numel() * 2 for shard in shard_tensors.values() for p in shard.values())
 index = {"metadata": {"total_size": total_size}, "weight_map": weight_map}
-logging.debug(f"Total size in bytes of the model: {total_size}")
-
-os.makedirs(output_folder, exist_ok=True)
-logging.debug("Created folder to store the weights.")
-
 with open(f"./{output_folder}/model.safetensors.index.json", "w") as f:
     f.write(json.dumps(index, indent=4) + "\n")
 logging.debug("Created model config file for the resharded model.")
-
-for shard, tensors in shard_tensors.items():
-    if tensors:
-        logging.debug(f"Saving tensors to {shard}")
-        save_file(tensors, f"./{output_folder}/{shard}", metadata={"format": "pt"})
-        del tensors
-
-logging.info("Processing completed.")
